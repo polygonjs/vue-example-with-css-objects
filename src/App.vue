a51993d33015b89dff06b29a18fac53b4ae7861b
Guillaume Fradin <guillaumefradin@gmail.com>
2023-03-28 15:10:02 +0100

    init project and create polygonjs scene

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Scene01 from "./polygonjs/scenes/Scene01.vue";
+</script>
+
+<template>
+	<Scene01 />
+	<a class="page-title" href="https://polygonjs.com">
+		<img class="page-title-logo" src="/polygonjs.png" />
+		<span class="page-title-text">Polygon + Vue + TS</span>
+	</a>
+</template>
+
+<style scoped></style>
---
+++ src/polygonjs/scenes/scene_01/autogenerated/loadSceneData.js
@@ -0,0 +1,40 @@
+import { SceneDataManifestImporter } from "@polygonjs/polygonjs/dist/src/engine/io/manifest/import/SceneData";
+const manifest = {
+  properties: "1680012487487",
+  root: "1675552563896",
+  nodes: {
+    geo1: "1680012487487",
+    "geo1/MAT": "1675552563896",
+    ground: "1680012487487",
+    "ground/MAT": "1675552563896",
+    "ground/MAT/meshStandardBuilder1": "1680012487487",
+    COP: "1675552563896",
+    lights: "1680012487487",
+    cameras: "1680012487487",
+    "cameras/cameraControls1": "1680012563989",
+    "cameras/cameraCSSRenderer1": "1680012487487",
+    CSSObjects: "1680012563989",
+  },
+  shaders: {
+    "/ground/MAT/meshStandardBuilder1": {
+      vertex: "1675552563896",
+      fragment: "1675552563896",
+      "customDepthMaterial.vertex": "1675552563896",
+      "customDepthMaterial.fragment": "1675552563896",
+      "customDistanceMaterial.vertex": "1675552563896",
+      "customDistanceMaterial.fragment": "1675552563896",
+      "customDepthDOFMaterial.vertex": "1675552563896",
+      "customDepthDOFMaterial.fragment": "1675552563896",
+    },
+  },
+};
+
+export const loadSceneData_scene_01 = async (options = {}) => {
+  const sceneDataRoot = options.sceneDataRoot || "./polygonjs/scenes";
+  return await SceneDataManifestImporter.importSceneData({
+    sceneName: "scene_01",
+    urlPrefix: sceneDataRoot + "/scene_01",
+    manifest: manifest,
+    onProgress: options.onProgress,
+  });
+};
